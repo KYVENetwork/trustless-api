039279f68692f51fb90971618d444aab8f112e3a
christopherbrumm <christopherbrumm2000@gmail.com>
2024-08-23 17:27:16 +0200

    feat: handle response without proof for Tendermint Indexer

--- files/local.go
+++ files/local.go
@@ -16,25 +16,35 @@ import (
 
 type SaveLocalFileInterface struct{}
 
-func (saveFile *SaveLocalFileInterface) Save(dataitem *types.TrustlessDataItem) (SavedFile, error) {
+func (saveFile *SaveLocalFileInterface) Save(dataItem *types.TrustlessDataItem, proofAttached bool) (SavedFile, error) {
+	var b []byte
+	var err error
 
 	// unmarshal item
-	b, err := json.Marshal(dataitem)
+	if proofAttached {
+		b, err = json.Marshal(dataItem)
 
-	if err != nil {
-		return SavedFile{}, err
+		if err != nil {
+			return SavedFile{}, err
+		}
+	} else {
+		b, err = json.Marshal(dataItem.ValueWithoutProof)
+
+		if err != nil {
+			return SavedFile{}, err
+		}
 	}
 
 	path := viper.GetString("storage.path")
-	dir := fmt.Sprintf("%v/%v/%v", path, dataitem.PoolId, dataitem.BundleId)
+	dir := fmt.Sprintf("%v/%v/%v", path, dataItem.PoolId, dataItem.BundleId)
 
 	// create directories if we need them
 	err = os.MkdirAll(dir, os.ModePerm)
 	if err != nil {
 		return SavedFile{}, err
 	}
-	var filepath string
-	filename := utils.GetUniqueDataitemName(dataitem)
+	var filePath string
+	filename := utils.GetUniqueDataitemName(dataItem)
 	switch viper.GetString("storage.compression") {
 	case "gzip":
 		var compressed bytes.Buffer
@@ -46,13 +56,13 @@ func (saveFile *SaveLocalFileInterface) Save(dataitem *types.TrustlessDataItem) 
 			return SavedFile{}, err
 		}
 		b = compressed.Bytes()
-		filepath = fmt.Sprintf("%v/%v.gz", dir, filename)
+		filePath = fmt.Sprintf("%v/%v.gz", dir, filename)
 	default:
-		filepath = fmt.Sprintf("%v/%v.json", dir, filename)
+		filePath = fmt.Sprintf("%v/%v.json", dir, filename)
 	}
 
 	// create the file
-	file, err := os.Create(filepath)
+	file, err := os.Create(filePath)
 	if err != nil {
 		return SavedFile{}, err
 	}
@@ -62,16 +72,16 @@ func (saveFile *SaveLocalFileInterface) Save(dataitem *types.TrustlessDataItem) 
 		return SavedFile{}, err
 	}
 
-	return SavedFile{Type: LocalFile, Path: filepath}, nil
+	return SavedFile{Type: LocalFile, Path: filePath}, nil
 }
 
-// loads a trustless dataitem from the local device storage
-// if the file ends with .gz it will automatically be decompressed
-func LoadLocalFile(link string) (types.TrustlessDataItem, error) {
+// LoadLocalFile loads a trustless data item from the local device storage.
+// If the file ends with .gz it will automatically be decompressed.
+func LoadLocalFile(link string) (json.RawMessage, error) {
 
 	file, err := os.ReadFile(link)
 	if err != nil {
-		return types.TrustlessDataItem{}, err
+		return json.RawMessage{}, err
 	}
 
 	switch filepath.Ext(link) {
@@ -79,23 +89,15 @@ func LoadLocalFile(link string) (types.TrustlessDataItem, error) {
 		var out bytes.Buffer
 		r, err := gzip.NewReader(bytes.NewBuffer(file))
 		if err != nil {
-			return types.TrustlessDataItem{}, err
+			return json.RawMessage{}, err
 		}
 		defer r.Close()
 
 		if _, err := io.Copy(&out, r); err != nil {
-			return types.TrustlessDataItem{}, err
+			return json.RawMessage{}, err
 		}
 
 		file = out.Bytes()
 	}
-
-	var dataItem types.TrustlessDataItem
-
-	err = json.Unmarshal(file, &dataItem)
-	if err != nil {
-		return types.TrustlessDataItem{}, err
-	}
-
-	return dataItem, nil
+	return file, nil
 }
