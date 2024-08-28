870ba127e1452852e3ace05539f661a15f2fb043
Christopher Brumm <97845034+christopherbrumm@users.noreply.github.com>
2024-08-28 17:26:05 +0200

    feat: disable proof with global param (#17)

--- server/server.go
+++ server/server.go
@@ -5,6 +5,8 @@ import (
 	_ "embed"
 	"encoding/json"
 	"fmt"
+	"github.com/KYVENetwork/trustless-api/files"
+	"github.com/KYVENetwork/trustless-api/types"
 	"html"
 	"html/template"
 	"io"
@@ -14,14 +16,11 @@ import (
 
 	"github.com/KYVENetwork/trustless-api/config"
 	"github.com/KYVENetwork/trustless-api/db"
-	"github.com/KYVENetwork/trustless-api/files"
 	"github.com/KYVENetwork/trustless-api/indexer"
-	"github.com/KYVENetwork/trustless-api/types"
 	"github.com/KYVENetwork/trustless-api/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	cachecontrol "go.eigsys.de/gin-cachecontrol/v2"
-	"gopkg.in/yaml.v3"
 )
 
 var (
@@ -64,7 +63,7 @@ func StartApiServer() *ApiServer {
 		pools = append(pools, serverPool)
 	}
 
-	openapiPaths, err := GenerateOpenApi(pools)
+	openapiPaths, err := generateOpenApi(pools)
 	if err != nil {
 		logger.Error().Str("err", err.Error()).Msg("failed to generate openapi")
 	}
@@ -119,7 +118,7 @@ func StartApiServer() *ApiServer {
 					})
 					return
 				}
-				apiServer.GetIndex(ctx, currentAdapter, indexString, indexId)
+				apiServer.getIndex(ctx, currentAdapter, indexString, indexId)
 			})
 		}
 	}
@@ -131,105 +130,15 @@ func StartApiServer() *ApiServer {
 	return apiServer
 }
 
-func GenerateOpenApi(pools []ServePool) ([]byte, error) {
-	paths := map[string]interface{}{}
-
-	for _, p := range pools {
-		adapter := p.Adapter
-		indexer := adapter.GetIndexer()
-
-		for prefix, value := range indexer.GetBindings() {
-
-			path := map[string]interface{}{}
-			path["tags"] = []string{p.Slug}
-
-			parameters := []map[string]interface{}{}
-			for _, param := range value.QueryParameter {
-
-				if len(param.Parameter) != len(param.Description) {
-					logger.Error().Msg("parameter and description length mismatch")
-					continue
-				}
-
-				for i, queryName := range param.Parameter {
-					currentParameter := map[string]interface{}{}
-					currentParameter["name"] = queryName
-					currentParameter["in"] = "query"
-					currentParameter["description"] = param.Description[i]
-					currentParameter["required"] = false
-					currentParameter["schema"] = map[string]interface{}{
-						"type": "string",
-					}
-					parameters = append(parameters, currentParameter)
-				}
-			}
-
-			path["parameters"] = parameters
-
-			var headers map[string]interface{}
-
-			if p.ProofAttached {
-				headers = map[string]interface{}{
-					"x-kyve-proof": map[string]interface{}{
-						"description": "KYVE Data Item Inclusion Proof Base64 encoded.",
-						"schema": map[string]string{
-							"type":    "string",
-							"example": "AIQAAAA...Jhhf6ut",
-						},
-					},
-				}
-			}
-
-			path["responses"] = map[int32]interface{}{
-				http.StatusOK: map[string]interface{}{
-					"description": "successful operation",
-					"content": map[string]interface{}{
-						"application/json": map[string]interface{}{
-							"schema": map[string]string{
-								"$ref": fmt.Sprintf("#/components/schemas/%v", value.Schema),
-							},
-						},
-					},
-					"headers": headers,
-				},
-				http.StatusNotFound: map[string]interface{}{
-					"description": "not found",
-					"content": map[string]interface{}{
-						"application/json": map[string]interface{}{
-							"schema": map[string]string{
-								"$ref": "#/components/schemas/Error",
-							},
-						},
-					},
-				},
-			}
-			// because we only support get requests we set the method to get
-			paths[fmt.Sprintf("/%v%v", p.Slug, prefix)] = map[string]interface{}{
-				"get": path,
-			}
-		}
-	}
-
-	ymlString, err := yaml.Marshal(map[string]interface{}{
-		"paths": paths,
-	})
-	if err != nil {
-		logger.Error().Str("err", err.Error()).Msg("failed to marshal paths")
-		return nil, err
-	}
-
-	return ymlString, nil
-}
-
 func (apiServer *ApiServer) findSelectedParameter(c *gin.Context, params *[]types.ParameterIndex) (string, int, error) {
 	// iterate over all params
 	// select the one where all params have a value set and return the build string from the parameter
 	for _, param := range *params {
 
-		query := []string{}
-		for _, queryName := range param.Parameter {
-			if c.Query(queryName) != "" {
-				query = append(query, c.Query(queryName))
+		var query []string
+		for _, parameterName := range param.Parameter {
+			if c.Query(parameterName) != "" {
+				query = append(query, c.Query(parameterName))
 			}
 		}
 
@@ -242,12 +151,12 @@ func (apiServer *ApiServer) findSelectedParameter(c *gin.Context, params *[]type
 	return "", 0, fmt.Errorf("wrong parameter")
 }
 
-// GetIndex will search the database for the given query and serve the correct data item if one is found
+// getIndex will search the database for the given query and serve the correct data item if one is found
 // if the desired data item does not exist it serves an error
 //
 // `index` - is the name of the index that will be used e. g. block_height
 // `indexId` - is the corresponding Id for the key e. g. block_height -> 0
-func (apiServer *ApiServer) GetIndex(c *gin.Context, adapter db.Adapter, index string, indexId int) {
+func (apiServer *ApiServer) getIndex(c *gin.Context, adapter db.Adapter, index string, indexId int) {
 	file, err := adapter.Get(indexId, index)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{
@@ -258,7 +167,7 @@ func (apiServer *ApiServer) GetIndex(c *gin.Context, adapter db.Adapter, index s
 	apiServer.resolveFile(c, file)
 }
 
-// serves the content of a SavedFile
+// resolveFile serves the content of a SavedFile
 func (apiServer *ApiServer) resolveFile(c *gin.Context, file files.SavedFile) {
 
 	var rawFile []byte
@@ -296,7 +205,6 @@ func (apiServer *ApiServer) resolveFile(c *gin.Context, file files.SavedFile) {
 }
 
 func (apiServer *ApiServer) serveFile(c *gin.Context, file []byte) {
-
 	var trustlessDataItem types.TrustlessDataItem
 	err := json.Unmarshal(file, &trustlessDataItem)
 	if err != nil {
@@ -307,8 +215,16 @@ func (apiServer *ApiServer) serveFile(c *gin.Context, file []byte) {
 	}
 
 	// only send the proof if it is attached
+	proofValue, proofParamExists := c.GetQuery("proof")
+
 	if trustlessDataItem.Proof != "" {
-		c.Header("x-kyve-proof", trustlessDataItem.Proof)
+		if proofParamExists {
+			if proofValue != "false" {
+				c.Header("x-kyve-proof", trustlessDataItem.Proof)
+			}
+		} else {
+			c.Header("x-kyve-proof", trustlessDataItem.Proof)
+		}
 	}
 	c.JSON(http.StatusOK, trustlessDataItem.Value)
 }
---
+++ server/utils.go
@@ -0,0 +1,107 @@
+package server
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"net/http"
+)
+
+// generateOpenApi generates the OpenAPI spec as yaml file
+func generateOpenApi(pools []ServePool) ([]byte, error) {
+	paths := map[string]interface{}{}
+
+	for _, p := range pools {
+		adapter := p.Adapter
+		adapterIndexer := adapter.GetIndexer()
+
+		for prefix, value := range adapterIndexer.GetBindings() {
+
+			path := map[string]interface{}{}
+			path["tags"] = []string{p.Slug}
+
+			var parameters []map[string]interface{}
+			for _, param := range value.QueryParameter {
+				if len(param.Parameter) != len(param.Description) {
+					logger.Error().Msg("parameter and description length mismatch")
+					continue
+				}
+
+				for i, parameterName := range param.Parameter {
+					currentParameter := map[string]interface{}{}
+					currentParameter["name"] = parameterName
+					currentParameter["in"] = "query"
+					currentParameter["description"] = param.Description[i]
+					currentParameter["required"] = false
+					currentParameter["schema"] = map[string]interface{}{
+						"type": "string",
+					}
+					parameters = append(parameters, currentParameter)
+				}
+			}
+
+			parameters = append(parameters, map[string]interface{}{
+				"name":        "proof",
+				"in":          "query",
+				"description": "disable KYVE Proof with `false`",
+				"required":    false,
+				"schema": map[string]interface{}{
+					"type": "string",
+				},
+			})
+
+			path["parameters"] = parameters
+
+			var headers map[string]interface{}
+
+			if p.ProofAttached {
+				headers = map[string]interface{}{
+					"x-kyve-proof": map[string]interface{}{
+						"description": "KYVE Data Item Inclusion Proof Base64 encoded.",
+						"schema": map[string]string{
+							"type":    "string",
+							"example": "AIQAAAA...Jhhf6ut",
+						},
+					},
+				}
+			}
+
+			path["responses"] = map[int32]interface{}{
+				http.StatusOK: map[string]interface{}{
+					"description": "successful operation",
+					"content": map[string]interface{}{
+						"application/json": map[string]interface{}{
+							"schema": map[string]string{
+								"$ref": fmt.Sprintf("#/components/schemas/%v", value.Schema),
+							},
+						},
+					},
+					"headers": headers,
+				},
+				http.StatusNotFound: map[string]interface{}{
+					"description": "not found",
+					"content": map[string]interface{}{
+						"application/json": map[string]interface{}{
+							"schema": map[string]string{
+								"$ref": "#/components/schemas/Error",
+							},
+						},
+					},
+				},
+			}
+			// because we only support get requests we set the method to get
+			paths[fmt.Sprintf("/%v%v", p.Slug, prefix)] = map[string]interface{}{
+				"get": path,
+			}
+		}
+	}
+
+	ymlString, err := yaml.Marshal(map[string]interface{}{
+		"paths": paths,
+	})
+	if err != nil {
+		logger.Error().Str("err", err.Error()).Msg("failed to marshal paths")
+		return nil, err
+	}
+
+	return ymlString, nil
+}
