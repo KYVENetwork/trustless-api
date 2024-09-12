dccb2e5932c669e50fa7387121945f6ed198869e
Jamin <57451149+GitInVisualStudio@users.noreply.github.com>
2024-09-12 08:51:15 +0200

    chore: default error message (#24)

--- indexer/helper/default.go
+++ indexer/helper/default.go
@@ -3,5 +3,8 @@ package helper
 type DefaultIndexer struct{}
 
 func (d *DefaultIndexer) GetErrorResponse(message string, data any) any {
-	return nil
+	return map[string]any{
+		"error":   message,
+		"message": data,
+	}
 }
